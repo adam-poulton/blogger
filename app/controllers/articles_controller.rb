2419f43bb4f85ebee999b2406f715c67ddff5b6e
adam-poulton <adam.poulton5@gmail.com>
2019-12-13 13:31:26 +1100

    Add article form validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,11 +18,13 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
 
-    flash.notice = "Article '#{@article.title}' Created!"
-
-    redirect_to article_path(@article)
+    if @article.save
+      redirect_to article_path(@article)
+      flash.notice = "Article '#{@article.title}' Created!"
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -31,11 +33,13 @@ class ArticlesController < ApplicationController
 
   def update
     @article = Article.find(params[:id])
-    @article.update(article_params)
 
-    flash.notice = "Article '#{@article.title}' Updated!"
-
-    redirect_to article_path(@article)
+    if @article.update(article_params)
+      flash.notice = "Article '#{@article.title}' Updated!"
+      redirect_to article_path(@article)
+    else 
+      render 'edit'
+    end
   end
 
   def destroy
